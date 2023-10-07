25e054152e613af091f460d91b4bdba6a2604be1
binhk <binhkhuu90@gmail.com>
2023-10-07 16:15:00 +0800

    partial classes

---
+++ PartialClasses/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+var test = new PartialMethodsDemo();
+Console.WriteLine(test.ToString());
+
+
+partial class PartialMethodsDemo
+{
+    public PartialMethodsDemo()
+    {
+        OnConstruction();
+    }
+    public override string ToString()
+    {
+        string ret = "Original return value";
+        CustomizeToString(ref ret);
+        return ret;
+    }
+    partial void OnConstruction();
+    // Because OnConstruction is never implemented, it's completely removed by the compiler.
+    // examine the .dll
+    partial void CustomizeToString(ref string text);
+}
+
+partial class PartialMethodsDemo
+{
+    partial void CustomizeToString(ref string text)
+    {
+        text += " - customized!";
+    }
+}
+
