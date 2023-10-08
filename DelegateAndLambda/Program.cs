abc6075d759c39308460967ed9b2aca1934c6989
binhk <binhkhuu90@gmail.com>
2023-10-08 10:53:49 +0800

    delegates

---
+++ DelegateAndLambda/Program.cs
@@ -0,0 +1,33 @@
+﻿//Delegate
+using MyDelegate;
+
+static void NotifyCallback(string str)
+{
+    Console.WriteLine(str);
+}
+// Paramter is the method group 'NotifyCallback'
+NotifyCallback del1 = new NotifyCallback(NotifyCallback);
+del1("ugh");
+
+// able to call correct overload
+NotifyCallbackOverload del2 = new NotifyCallbackOverload(MyClass.NotifyCallback);
+del2(1);
+
+delegate void NotifyCallback(string str);
+delegate void NotifyCallbackOverload(int i); //set to overloaded int signiture
+
+namespace MyDelegate
+{
+    static class MyClass
+    {
+        public static void NotifyCallback(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        public static void NotifyCallback(int i) 
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
