96dd547f89d921d7807fb99e0564c5d090ba646e
binhk <binhkhuu90@gmail.com>
2023-10-08 14:19:32 +0800

    add transparent identifiers

--- ExpressionTrees/Program.cs
+++ ExpressionTrees/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System.Linq.Expressions;
-
+using System.Linq;
 // this is not allowed because statement body is statement body
 //Expression<Func<int,int,int>> addr = (x, y) => { return x + y; };
 
@@ -17,4 +17,52 @@ myList.Select(x => x + 1).ToList();
 foreach (var item in myList)
 {
     Console.WriteLine($"MyList item: {item}");
+}
+
+// Query Expression
+IEnumerable<int> queryExpression = from number in myList
+                         where number > 1
+                         orderby number
+                         select number;
+
+// equivilent Linq Chaining
+IEnumerable<int> query = myList.Where(x => x > 1)
+    .OrderBy(x => x)
+    .Select(x => x);
+
+
+
+foreach (var item in queryExpression)
+{
+    Console.WriteLine($"queryExpression number: {item}");
+}
+
+foreach (var item in query)
+{
+    Console.WriteLine($"queryExpression number: {item}");
+}
+
+
+// TransparentIdentifier * for let variable complier is doing the heavy lifting when creating the scoped variables
+var myQueryExpression = from number in myList
+                        let identifier = number * 2
+                        where identifier > 5
+                        orderby identifier
+                        select $"ID {identifier}  Number {number}";
+
+// Equivalent in Linq Chaining you do the heavy lifting creating the transparent identifiers
+var myQuery = myList.Select(num => new { num, identifier = num * 2 })
+    .Where(temp => temp.identifier > 5)
+    .OrderBy(temp => temp.identifier)
+    .Select(temp => $"ID {temp.identifier}  Number {temp.num}");
+
+
+foreach(var item in myQueryExpression)
+{
+    Console.WriteLine(item );
+}
+
+foreach (var item in myQuery)
+{
+    Console.WriteLine(item);
 }
