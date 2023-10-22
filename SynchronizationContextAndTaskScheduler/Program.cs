1b2003d7046057a381cf0699a2db252dcf4c554e
binhk <binhkhuu90@gmail.com>
2023-10-22 11:40:30 +0800

    console app sync context

---
+++ SynchronizationContextAndTaskScheduler/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Use Synchronization context to execute continuation
+SynchronizationContext sc = SynchronizationContext.Current;
+await Task.Delay(TimeSpan.FromSeconds(1)).ContinueWith(t =>
+{
+   
+    sc.Post(delegate
+    {
+        Console.WriteLine("ugh");
+    },null);
+});
+
+
+// Marshall the current SynchronizationContext from the TaskScheduler.
+await Task.Delay(TimeSpan.FromSeconds(1)).ContinueWith(t =>
+{
+    Console.WriteLine("ugh");
+}, TaskScheduler.FromCurrentSynchronizationContext());
+
